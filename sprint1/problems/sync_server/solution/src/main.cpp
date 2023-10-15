6a0f4c3b33ac22a766d8c83348e8cbdf41d279c1
ppvartm <artm.ppv28@gmail.com>
2023-10-15 18:55:22 +0300

    Added a solution of sync_serv train-task

--- sprint1/problems/sync_server/solution/src/main.cpp
+++ sprint1/problems/sync_server/solution/src/main.cpp
@@ -20,6 +20,11 @@ namespace http = beast::http;
 using StringRequest = http::request<http::string_body>;
 using StringResponse = http::response<http::string_body>;
 
+struct ContentType {
+    ContentType() = delete;
+    constexpr static beast::string_view TEXT_HTML = "text/html";
+};
+
 //Функция для чтения запроса от клиента
 std::optional<StringRequest> ReadRequest(tcp::socket& socket, beast::flat_buffer& buffer) {
     beast::error_code ec;
@@ -40,9 +45,37 @@ void DumpRequest(const StringRequest& req)
         std::cout << "  "sv << header.name_string() << ": "sv << header.value() << "\n"sv;
 }
 
+StringResponse MakeStringResponse(http::status status, std::string_view body, unsigned http_version,
+    bool keep_alive,
+    std::string_view content_type = ContentType::TEXT_HTML) {
+    StringResponse response(status, http_version);
+    response.set(http::field::content_type, content_type);
+    response.body() = body;
+    response.content_length(body.size());
+    response.keep_alive(keep_alive);
+    return response;
+}
+
+
+
 StringResponse HandleRequest(StringRequest&& req) //понять почему по правой ссылке
 {
-
+    
+    const auto text_response = [&req](http::status status, std::string_view text) {
+        return MakeStringResponse(status, text, req.version(), req.keep_alive());
+        };
+    if (req.method_string() == "GET")
+    {
+        std::string str1 = "<strong>Hello, ";
+        std::string targ = static_cast<std::string> (req.target().substr(1, req.target().size() - 1));
+        std::string str2 = "</strong>";
+        return text_response(http::status::ok, str1 + targ + str2);
+    }
+    if (req.method_string() == "HEAD")
+        return text_response(http::status::ok, "");
+    auto response = text_response(http::status::method_not_allowed, "Invalid method");
+    response.set(http::field::allow, "GET, HEAD");
+    return response;
 }
 
 
@@ -54,13 +87,16 @@ void HandleConnection(tcp::socket& socket, RequestHandler&& HandleRequest)
 
         while (auto request = ReadRequest(socket, buffer)) {
             DumpRequest(request.value());
-            StringResponse = HandleRequest(std::move(request).value());
+            StringResponse response = HandleRequest(std::move(request).value());
+            http::write(socket, response);
+            if (response.need_eof()) break;
         }
     }
     catch (std::exception& ex) {
-
+        std::cerr << ex.what() << "\n"sv;
     }
-
+    beast::error_code ec;
+    socket.shutdown(tcp::socket::shutdown_send, ec);
 }
 int main() {
     // Выведите строчку "Server has started...", когда сервер будет готов принимать подключения
@@ -74,7 +110,7 @@ int main() {
     {
         tcp::socket socket(io_context);
         acceptor.accept(socket);
-        std::thread t([](tcp::socket socket) { HandleConnection(socket); }, std::move(socket));
+        std::thread t([](tcp::socket socket) { HandleConnection(socket, HandleRequest); }, std::move(socket));
         t.detach();
     }
 
