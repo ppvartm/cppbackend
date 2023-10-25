3b6519ffff3b5f9f7753833fd87891b287aa1ce1
ppvartm <artm.ppv28@gmail.com>
2023-10-25 23:30:11 +0300

    Add a solution of map-json train-task

--- sprint1/problems/map_json/solution/src/request_handler.h
+++ sprint1/problems/map_json/solution/src/request_handler.h
@@ -20,7 +20,7 @@ public:
     
     StringResponse MakeStringResponse(http::status status, std::string_view body, unsigned http_version,
         bool keep_alive,
-        boost::beast::string_view content_type = "text/html") {
+        boost::beast::string_view content_type = "application/json") {
         StringResponse response(status, http_version);
         response.set(http::field::content_type, content_type);
         response.body() = body;
@@ -38,21 +38,57 @@ public:
         const auto text_response = [&req, this](http::status status, std::string_view text) {
         return this->MakeStringResponse(status, text, req.version(), req.keep_alive());
         };
-        if (req.method_string() == "GET")
+        if ((req.method_string() == "GET")&& (static_cast<std::string>(req.target())=="/api/v1/maps"))
         {
-            std::string str1 = "Hello, ";
-            std::string targ = static_cast<std::string> (req.target().substr(1, req.target().size() - 1));
-            std::string str2 = "";
-           send(text_response(http::status::ok, str1 + targ + str2));
+            std::string answ="[";
+            for (auto& it: game_.GetMaps())
+            {
+                answ += "{\"id\": \"" + *it.GetId() + "\", \"name\": \"" + it.GetName() + "\"},";
+            }
+            answ.erase(answ.size()-1, answ.size());
+            answ += "]";
+           send(text_response(http::status::ok, answ));
            return;
         }
-        if (req.method_string() == "HEAD") {
-            send(text_response(http::status::ok, ""));
-            return;
+        if ((req.method_string() == "GET") && (static_cast<std::string>(req.target()).substr(0, 16) == "/api/v1/maps/map"))
+        {
+            std::string id = static_cast<std::string>(req.target()).substr(13);
+            auto map = game_.FindMap(model::Map::Id(id));
+            if (map) {
+                std::string answ = "{\n\"id\": \"" + *(map->GetId()) + "\",\n" +
+                    "\"name\": \"" + map->GetName() + "\",\n" +
+                    "\"roads\": [\n";
+                for (auto& it : map->GetRoads()) {
+                    answ += "{ \"x0\": " + std::to_string(it.GetStart().x) + ", \"y0\":" + std::to_string(it.GetStart().y) + ",";
+                    if (it.IsHorizontal()) answ += "\"x1\":" + std::to_string(it.GetEnd().x) + "}\n";
+                    if (it.IsVertical()) answ += "\"y1\":" + std::to_string(it.GetEnd().y) + "},\n";
+                }
+                answ.erase(answ.size() - 2, answ.size());
+                answ += "\n],\n \"buildings\": [\n";
+                for (auto& it : map->GetBuildings()) {
+                    answ += "\"x\": " + std::to_string(it.GetBounds().position.x) + ", \"y\": " + std::to_string(it.GetBounds().position.y) + ", \"w\": " +
+                        std::to_string(it.GetBounds().size.width) + ", \"h\": " + std::to_string(it.GetBounds().size.height) + "},\n";
+                }
+                answ.erase(answ.size() - 2, answ.size());
+                answ += "\n],\n \"offices\": [\n";
+                for (auto& it : map->GetOffices()) {
+                    answ += "{ \"id\": \"" + *it.GetId() + "\", \"x\": " + std::to_string(it.GetPosition().x) + ", \"y\":" + std::to_string(it.GetPosition().y) +
+                        ", \"offsetX\": " + std::to_string(it.GetOffset().dx) + ", \"offsetY\": " + std::to_string(it.GetOffset().dy) + "},\n";
+                }
+                answ.erase(answ.size() - 2, answ.size());
+                answ += "\n]\n}";
+                send(text_response(http::status::ok, answ));
+                return;
+            }
+            else {
+                std::string answ = "{\n  \"code\": \"mapNotFound\",\n  \"message\": \"Map not found\"\n}";
+                send(text_response(http::status::method_not_allowed, answ));
+                return;
+            }
         }
-        auto response = text_response(http::status::method_not_allowed, "Invalid method");
-        response.set(http::field::allow, "GET, HEAD");
-      send (response);
+       std::string answ = "{\n  \"code\": \"badRequest\",\n  \"message\": \"Bad request\"\n}";
+       send(text_response(http::status::bad_request, answ));
+       return;
     }
 
 private:
@@ -60,22 +96,3 @@ private:
 };
 
 }  // namespace http_handler
-
-//StringResponse HandleRequest(StringRequest&& req) {
-//    const auto text_response = [&req](http::status status, std::string_view text) {
-//        return MakeStringResponse(status, text, req.version(), req.keep_alive());
-//        };
-//    if (req.method_string() == "GET")
-//    {
-//        std::string str1 = "Hello, ";
-//        std::string targ = static_cast<std::string> (req.target().substr(1, req.target().size() - 1));
-//        std::string str2 = "";
-//        return text_response(http::status::ok, str1 + targ + str2);
-//    }
-//    if (req.method_string() == "HEAD")
-//        return text_response(http::status::ok, "");
-//    auto response = text_response(http::status::method_not_allowed, "Invalid method");
-//    response.set(http::field::allow, "GET, HEAD");
-//    return response;
-//    // return MakeStringResponse(http::status::ok, "OK"sv, req.version(), req.keep_alive());
-//}
