3a756e4d65dd98408ccb409621dfeb17e6005dda
ppvartm <artm.ppv28@gmail.com>
2023-11-19 16:16:27 +0300

    Add a comand_line train-task solution; v.1

---
+++ sprint2/problems/command_line/solution/src/app.h
@@ -0,0 +1,194 @@
+#pragma once
+#include <memory>
+#include <random>
+#include <unordered_map>
+#include "model.h"
+#include <iostream>
+#include <boost/functional/hash.hpp>
+
+namespace app {
+    using Token = std::string;
+
+
+	class Player {
+	public:
+        Player() = delete;
+        Player(std::shared_ptr<model::Dog> dog, std::shared_ptr<model::GameSession> game_session):
+            dog_(dog), 
+            game_session_(game_session){
+            game_session->AddDog(dog_);
+        }
+
+        model::Dog::Id GetDogId() {
+            return dog_->GetId();
+        }
+        const std::shared_ptr<model::GameSession> GetGameSession() const {
+            return game_session_;
+        }
+        void SetDogSpeed(model::DogSpeedFromJson speed_horizontal, model::DogSpeedFromJson speed_vertical) {
+            dog_->SetSpeed({speed_horizontal, speed_vertical});
+        }
+        void SetDogDirection(const std::string& dir) {
+            dog_->SetDirection(dir);
+        }
+        std::vector<model::Road> GetRoadsWithDog() {
+            std::vector<model::Road> valid_roads;
+            for (auto& road : game_session_->GetMap().GetRoads()) {
+                if (road.IsHorizontal()) {
+                    if((dog_->GetPosition().y >= road.GetStart().y - 0.4 && dog_->GetPosition().y <= road.GetEnd().y + 0.4)&&
+                        ((dog_->GetPosition().x >= road.GetStart().x - 0.4 && dog_->GetPosition().x <= road.GetEnd().x + 0.4) ||
+                         (dog_->GetPosition().x <= road.GetStart().x + 0.4 && dog_->GetPosition().x >= road.GetEnd().x - 0.4)))
+                            valid_roads.push_back(road);
+                    /*if (((dog_->GetPosition().x >= road.GetStart().x - 0.4 && dog_->GetPosition().y >= road.GetStart().y - 0.4) &&
+                        (dog_->GetPosition().x <= road.GetEnd().x + 0.4 && dog_->GetPosition().y <= road.GetEnd().y + 0.4))||
+                        ())
+                        */
+                }
+                else if (road.IsVertical()) {
+                    if ((dog_->GetPosition().x >= road.GetStart().x - 0.4 && dog_->GetPosition().x <= road.GetEnd().x + 0.4) &&
+                        ((dog_->GetPosition().y >= road.GetStart().y - 0.4 && dog_->GetPosition().y <= road.GetEnd().y + 0.4) ||
+                            (dog_->GetPosition().y <= road.GetStart().y + 0.4 && dog_->GetPosition().y >= road.GetEnd().y - 0.4)))
+                        valid_roads.push_back(road);
+
+
+                /*else if (road.IsVertical()) {
+                    if ((dog_->GetPosition().x >= road.GetStart().x - 0.4 && dog_->GetPosition().y >= road.GetStart().y - 0.4) &&
+                        (dog_->GetPosition().x <= road.GetEnd().x + 0.4 && dog_->GetPosition().y <= road.GetEnd().y + 0.4))
+                        valid_roads.push_back(road);*/
+                }
+            }
+                    return valid_roads;
+            
+        }
+        model::Position NewCorrectPosition(model::Position new_position) {
+            auto roads = GetRoadsWithDog();
+            auto answer = new_position;
+            for (auto& road : roads) {
+                if (road.IsHorizontal()) {
+                    if (new_position.y > road.GetStart().y + 0.4)
+                        answer.y = road.GetStart().y + 0.4;
+                    if (new_position.y < road.GetStart().y - 0.4)
+                        answer.y = road.GetStart().y - 0.4;
+
+                    if (road.GetStart().x < road.GetEnd().x) {
+                        if (new_position.x < road.GetStart().x - 0.4)
+                            answer.x = road.GetStart().x - 0.4;
+                        if (new_position.x > road.GetEnd().x + 0.4)
+                            answer.x = road.GetEnd().x + 0.4;
+                    }
+                    else {
+                        if (new_position.x > road.GetStart().x + 0.4)
+                            answer.x = road.GetStart().x + 0.4;
+                        if (new_position.x < road.GetEnd().x - 0.4)
+                            answer.x = road.GetEnd().x - 0.4;
+                    }
+                }
+                else {
+                    if (new_position.x > road.GetStart().x + 0.4)
+                        answer.x = road.GetStart().x + 0.4;
+                    if (new_position.x < road.GetStart().x - 0.4)
+                        answer.x = road.GetStart().x - 0.4;
+
+                    if (road.GetStart().y < road.GetEnd().y) {
+                        if (new_position.y < road.GetStart().y - 0.4)
+                            answer.y = road.GetStart().y - 0.4;
+                        if (new_position.y > road.GetEnd().y + 0.4)
+                            answer.y = road.GetEnd().y + 0.4; 
+                    }
+                    else {
+                        if (new_position.y > road.GetStart().y + 0.4)
+                            answer.y = road.GetStart().y + 0.4;
+                        if (new_position.y < road.GetEnd().y - 0.4)
+                            answer.y = road.GetEnd().y - 0.4;
+                    }
+                }
+                if (roads.size() == 1)
+                    return answer;
+                if (answer.x == new_position.x && answer.y == new_position.y)
+                    return answer;
+                if (road.IsHorizontal() && (dog_->GetDirection() == model::Direction::WEST || dog_->GetDirection() == model::Direction::EAST))
+                    return answer;
+                if (road.IsVertical() && (dog_->GetDirection() == model::Direction::SOUTH || dog_->GetDirection() == model::Direction::NORTH))
+                    return answer;
+                answer = new_position;
+              /*
+                if ((answer.x == new_position.x) && (answer.y == new_position.y))
+                    return new_position;
+                if (roads.size() == 1)
+                    return answer;
+                else if (&road == &(*roads.begin()))
+                    answer = new_position;*/
+            }
+            return answer;
+        }
+      
+
+        void MoveDog(double time_delta) {  //время в миллисикундах
+            model::Position new_position = {
+                dog_->GetPosition().x + dog_->GetSpeed().s_x * time_delta / 1000.,
+                dog_->GetPosition().y + dog_->GetSpeed().s_y * time_delta / 1000.
+            };
+            model::Position new_correct_position = NewCorrectPosition(new_position);
+            if((new_position.x != new_correct_position.x) || (new_position.y != new_correct_position.y))
+               SetDogSpeed(0.,0.);
+            dog_->SetPosition(new_correct_position);
+        }
+	private:
+        std::shared_ptr<model::Dog> dog_;
+		std::shared_ptr<model::GameSession> game_session_;
+		
+	};
+
+    class PlayerTokens {
+    private:
+
+        std::unordered_map<Token, std::shared_ptr<Player>> token_to_player_;
+
+        std::random_device random_device_;
+        std::mt19937_64 generator1_{ [this] {
+            std::uniform_int_distribution<std::mt19937_64::result_type> dist;
+            return dist(random_device_);
+        }() };
+        std::mt19937_64 generator2_{ [this] {
+            std::uniform_int_distribution<std::mt19937_64::result_type> dist;
+            return dist(random_device_);
+        }() };
+
+        Token GenerateToken();
+    public:
+        std::shared_ptr<Player> FindPlayerByToken(const Token& token) {
+            auto player = token_to_player_.find(token);
+            if (player != token_to_player_.end())
+                return player->second;
+            return nullptr;
+
+        }
+
+
+        Token AddPlayer(std::shared_ptr<Player> player) {
+            Token token = GenerateToken();
+            token_to_player_[token] = player;
+            return token;
+        }
+    };
+
+    class Players {
+    public:
+        std::shared_ptr<Player> Add(std::shared_ptr<model::Dog> dog, std::shared_ptr<model::GameSession> game_session) {
+            return players_[{dog->GetId(), *(game_session->GetMapId())}] = std::make_shared<Player>(dog, game_session);
+        }
+      
+        std::shared_ptr<Player> FindByDogAndMapId(model::Dog::Id dog_id, model::Map::Id map_id) {
+            return players_[{dog_id, *map_id}];
+        }
+
+        void MoveAllDogs(double time_delta) {
+            for (auto& player: players_)
+                player.second->MoveDog(time_delta);
+        }
+
+    private:
+        std::unordered_map<std::pair<uint64_t, std::string>, std::shared_ptr<Player>, boost::hash<std::pair<uint64_t, std::string>>> players_;
+
+    };
+}
---
+++ sprint2/problems/command_line/solution/src/main.cpp
@@ -0,0 +1,174 @@
+//#include "sdk.h"
+#include <boost/program_options.hpp>
+#include <boost/asio/signal_set.hpp>
+#include <boost/asio/io_context.hpp>
+#include <iostream>
+#include <thread>
+
+#include "json_loader.h"
+#include "request_handler.h"
+#include "http_server.h"
+
+#include "log.h"
+#include "app.h"
+
+
+
+
+using namespace std::literals;
+namespace net = boost::asio;
+namespace sys = boost::system;
+namespace po = boost::program_options;
+
+namespace {
+
+ // Запускает функцию fn на n потоках, включая текущий
+template <typename Fn>
+void RunWorkers(unsigned num_of_threads, const Fn& fn) {
+    num_of_threads = std::max(1u, num_of_threads);
+    std::vector<std::jthread> workers;
+    workers.reserve(num_of_threads - 1);
+    // Запускаем n-1 рабочих потоков, выполняющих функцию fn
+    while (--num_of_threads) {
+        workers.emplace_back(fn);
+    }
+    fn();
+
+}
+
+// структура параметров командной строки 
+struct Args {
+    std::string tick_period;
+    std::string config_file_path;
+    std::string static_dir_path;
+    std::string random_spawn;
+};
+
+std::optional<Args> ParseCommandLine(int argc, const char* const argv[]) {
+    po::options_description desc{ "All options"s };
+    Args args;
+    desc.add_options()
+        ("help,h", "Show help")
+        ("tick-period,t", po::value(&args.tick_period)->value_name("millisecondse"s), "Set tick period")
+        ("config-file,c", po::value(&args.config_file_path)->value_name("file"s), "Set config file path")
+        ("www-root,w", po::value(&args.static_dir_path)->value_name("dir"s), "Set static files root")
+        ("randomize-spawn-points", "Set static files root");
+
+    po::variables_map vm;
+    po::store(po::parse_command_line(argc, argv, desc), vm);
+    po::notify(vm);
+
+    if (vm.contains("help"s)) {
+        std::cout << desc;
+        return std::nullopt;
+    }
+
+    // Проверяем наличие опций src и dst
+    if (!vm.contains("tick-period"s)) {
+
+    }
+    if (!vm.contains("config-file"s)) {
+        throw std::runtime_error("Config file path is not specified"s);
+    }
+    if (!vm.contains("www-root"s)) {
+        throw std::runtime_error("Static dir path is not specified"s);
+    }
+    if (!vm.contains("randomize-spawn-points"s)) {
+       // args.random_spawn = "";
+    }
+    return args;
+  }
+}  // namespace
+
+
+int main(int argc, const char* argv[]) {
+ /*   if (argc != 3) {
+        std::cerr << "Usage: game_server <game-config-json>"sv << std::endl;
+        return EXIT_FAILURE;
+    }*/
+    try {
+       /* app::Players p;
+        model::Map::Id id{ static_cast<std::string>("2") };
+        p.FindByDogAndMapId(1, id);*/
+
+
+        logging::add_common_attributes();
+        logging::add_console_log(
+            std::clog,
+            logging::keywords::format = MyFormatter,
+            logging::keywords::auto_flush = true
+        );
+
+        auto args = ParseCommandLine(argc, argv);
+        std::cout << args->config_file_path << "\n";
+        std::cout << args->static_dir_path << "\n";
+        std::cout << args->random_spawn << "\n";
+
+
+        
+        std::filesystem::path path1{ args->config_file_path };
+        std::filesystem::path path2{ args->static_dir_path };
+        if (!std::filesystem::exists(path1))
+            throw std::runtime_error("Config file doesn't exist");
+        if (!std::filesystem::exists(path2))
+            throw std::runtime_error("Static files dir doesn't exist");
+
+      //  std::filesystem::path path1{ argv[1] };
+        path1 = std::filesystem::weakly_canonical(path1);
+      //  std::filesystem::path path2{ argv[2] };
+        path2 = std::filesystem::weakly_canonical(path2);
+     //    1. Загружаем карту из файла и построить модель игры
+        model::Game game = json_loader::LoadGame(path1);
+        if (args->random_spawn != "")
+            game.SetRandomSpawn();
+
+       // model::Game game = json_loader::LoadGame("../data/config.json");
+      //   2. Инициализируем io_context
+        const unsigned num_threads = std::thread::hardware_concurrency();
+        net::io_context ioc(num_threads);
+
+       //  3. Добавляем асинхронный обработчик сигналов SIGINT и SIGTERM
+        net::signal_set signals(ioc, SIGINT, SIGTERM);
+        signals.async_wait([&ioc](const sys::error_code& ec, [[maybe_unused]] int signal_number) {
+            if (!ec) {
+                ioc.stop();
+            }
+            });
+
+        
+      //   4. Создаём обработчик HTTP-запросов и связываем его с моделью игры
+        http_handler::RequestHandler handler {game};
+        handler.SetFilePath(path2);
+      //  handler.SetFilePath("../static");
+        http_handler::LoggingRequestHandler logging_handler(handler);
+
+       
+
+       //  5. Запустить обработчик HTTP-запросов, делегируя их обработчику запросов
+        const auto address = net::ip::make_address("0.0.0.0");
+        constexpr net::ip::port_type port = 8080;
+  /*      http_server::ServeHttp(ioc, {address, port}, [&handler](auto&& req, auto&& send) {
+            handler(std::forward<decltype(req)>(req), std::forward<decltype(send)>(send));
+        });*/
+
+        http_server::ServeHttp(ioc, { address, port }, [&logging_handler](auto&& req, auto&& send) {
+            logging_handler(std::forward<decltype(req)>(req), std::forward<decltype(send)>(send));
+            });
+        
+
+      //   Эта надпись сообщает тестам о том, что сервер запущен и готов обрабатывать запросы
+        std::cout << "Server has started..."sv << std::endl;
+        ServerStartLog(port, address);
+      //   6. Запускаем обработку асинхронных операций
+        RunWorkers(std::max(1u, num_threads), [&ioc] {
+            ioc.run();
+        });
+        ServerStopLog(0);
+        std::cin.get();
+    } catch (const std::exception& ex) {
+        ServerStopLog(EXIT_FAILURE, ex.what());
+      //  std::cout << ex.what() << std::endl;
+        return EXIT_FAILURE;
+    }
+    std::cin.get();
+}
